b72acbbde7f5defe115e0f69887688cd3d032c3b
ShravyaJakkani <jakkanishravya@gmail.com>
2026-06-25 18:46:20 +0530

    updated swagger file

--- src/main/java/com/campus/collab/config/OpenApiConfig.java
+++ src/main/java/com/campus/collab/config/OpenApiConfig.java
@@ -6,20 +6,15 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI campusCollabOpenAPI() {
-        Server server = new Server();
-        server.setUrl("http://localhost:9004");
-        server.setDescription("Development server");
 
         Contact contact = new Contact();
         contact.setEmail("info@campuscollab.com");
@@ -46,10 +41,9 @@ public class OpenApiConfig {
                 .addList("Bearer Authentication");
 
         return new OpenAPI()
-                .info(info)
-                .servers(List.of(server))
-                .addSecurityItem(securityRequirement)
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("Bearer Authentication", securityScheme));
+        .info(info)
+        .addSecurityItem(securityRequirement)
+        .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("Bearer Authentication", securityScheme));
     }
 }
